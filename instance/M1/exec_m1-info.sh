c80769f4545a9d417a8df95c6a3518c54d082182
Corentin <Corentin.Behuet@univ-angers.fr>
2023-03-10 10:02:23 +0100

    Ajout du M1, futur modification: modele java

---
+++ instance/M1/exec_m1-info.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+touch rules_m1-informatique_anglais-m1-informatique-s1.xml
+touch rules_m1-informatique_anglais-m1-informatique-s2.xml
+touch rules_m1-informatique_apprentissage-artificiel.xml
+touch rules_m1-informatique_communication-s1.xml
+touch rules_m1-informatique_communication-s2.xml
+touch rules_m1-informatique_conférences-professionnelles.xml
+touch rules_m1-informatique_design patterns.xml
+touch rules_m1-informatique_developpement-mobile-m1.xml
+touch rules_m1-informatique_docker.xml
+touch rules_m1-informatique_droit-m1.xml
+touch rules_m1-informatique_génie-logiciel.xml
+touch rules_m1-informatique_intelligence_artificielle_1.xml
+touch rules_m1-informatique_intelligence-artificielle-2.xml
+touch rules_m1-informatique_introduction-résolution-probleme.xml
+touch rules_m1-informatique_machine-learning.xml
+touch rules_m1-informatique_optimisation-combinatoire.xml
+touch rules_m1-informatique_optimisation-linéaire.xml
+touch rules_m1-informatique_programmation-parallele-m1.xml
+touch rules_m1-informatique_programmation-réseaux.xml
+touch rules_m1-informatique_programmation-système.xml
+touch rules_m1-informatique_recherche-automatique-information.xml
+touch rules_m1-informatique_representation-connaissances.xml
+touch rules_m1-informatique_réseau-m1-informatique.xml
+touch rules_m1-informatique_TALN.xml
+touch rules_m1-informatique_web-avancé.xml
+touch rules_m1-informatique_web-des-données.xml
